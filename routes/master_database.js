0be05b2909bffbf600adafcb32fd32088d04c100
Fefei Server <fefei@server.local>
2026-08-02 07:39:29 -0400

    Auto Code Backup : 02-08-2026 07:39 AM

--- routes/master_database.js
+++ routes/master_database.js
@@ -95,7 +95,7 @@ module.exports = function (pool, middlewares, helpers) {
         } catch (err) {
           console.error("Telegram Alert Error:", err.message);
         }
-      }, 30000); // 30 seconds batch window
+      }, 60000); // 60 seconds batch window
     }
   }
 
